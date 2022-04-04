72de984ccb71fd0cca8c7924d838c14ef15c7836
Nils <nils@sld.gs>
2022-04-04 17:10:32 +0200

    add: medium clap component, basic user interactivity

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Clap from '../assets/clap.svg';
+import styles from './index.css';
 
-const Clap = () => {
-  return <h1>Hello 01 Component</h1>
-}
+const initialState = {
+  count: 0,
+  countTotal: 267,
+};
 
-export default Clap
+const MediumClap = () => {
+  const [clapState, setClapState] = useState(initialState);
+  const { count, countTotal } = clapState;
+
+  const handleClapClick = () => {
+    setClapState((prevState) => ({
+      count: prevState.count + 1,
+      countTotal: prevState.countTotal + 1,
+    }));
+  };
+
+  return (
+    <button className={styles.clap} onClick={handleClapClick}>
+      <ClapIcon />
+      <ClapCount count={count} />
+      <ClapTotal countTotal={countTotal} />
+    </button>
+  );
+};
+
+/**
+ * Sub Components
+ */
+
+const ClapIcon = () => {
+  return <Clap className={styles.icon} />;
+};
+
+const ClapCount = ({ count }) => {
+  return <span className={styles.count}>+ {count}</span>;
+};
+
+const ClapTotal = ({ countTotal }) => {
+  return <span className={styles.total}>{countTotal}</span>;
+};
+
+export default MediumClap;
